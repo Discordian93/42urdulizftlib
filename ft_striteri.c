d88ce7ee98eb4c80e39875b55bd0b6d815fca49d
tekserf <estebaniza@gmail.com>
2023-11-13 10:38:17 +0100

    add putchar_fd, putendl_fd, putnbr_fd, putstr_fd, striteri, strmapi

---
+++ ft_striteri.c
@@ -0,0 +1,36 @@
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	size_t		len;
+	size_t		i;
+
+	len = ft_strlen(s);
+	i = 0;
+	while (i < len)
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
+/* 
+// Additional code for testing
+#include <stdio.h>
+#include <ctype.h>
+
+void to_upper(unsigned int i, char *c) {
+	printf("%i\n", i);
+    *c = toupper(*c);
+}
+
+int main() {
+    char s[] = "Hello, World!";
+    ft_striteri(s, to_upper);
+    printf("%s\n", s);  // Should print "HELLO, WORLD!"
+
+    char s2[] = "abc";
+    ft_striteri(s2, to_upper);
+    printf("%s\n", s2);  // Should print "ABC"
+
+    return 0;
+} */
